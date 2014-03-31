62703bdd5e86faff8087818e5ff9513723e7fa69
dominic-hickey <dominic.j.hickey@gmail.com>
2014-03-31 20:31:35 +0100

    Added site

---
+++ contact-form/contactthanks.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>The SafeHouse Hostel</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="css/base.css">
+	<link rel="stylesheet" href="css/skeleton.css">
+	<link rel="stylesheet" href="css/layout.css">
+	<link rel="stylesheet" href="css/fonts.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body>
+
+	<!-- Primary Page Layout
+	================================================== -->
+
+	<div class="container">
+	
+		<img src="images/logo.png" class="logo">	
+
+		<div class="main clearfix">
+			<div class="row eight columns">
+				<h2>Almost Ready to Stay</h2>
+				<p class="welcome">The Safe House is a new boutique hostel in Cardiff city centre offering superb customer service and a warm welcome to each visitor.</p>
+			</div>
+			<div class="row eight columns">
+				<h2>Thankyou for Submitting</h2>
+
+					<!-- CONTACT FORM !-->
+					<form method="post" action="contactengine.php">
+						<input type="text" name="Email" id="Email" placeholder="Enter your email address..." />
+						<input type="submit" name="submit" value="Send" class="submit-button" />
+					</form>
+
+			</div>
+		</div>
+
+	</div><!-- container -->
+
+
+
+
+</body>
+</html>
